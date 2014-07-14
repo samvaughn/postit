9b496e58d6ff0e59bb0cb7c0b0499fec9695609e
samuelvaughn <samuel.vaughn@octanner.com>
2014-07-14 08:03:37 -0600

    end of week 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  resources :posts, except: [:destroy] do
+    resources :comments
+  end
+  resources :categories, only [:new, :create, :show] do
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616205345) do
+ActiveRecord::Schema.define(version: 20140620153104) do
 
   create_table "categories", force: true do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "category_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
